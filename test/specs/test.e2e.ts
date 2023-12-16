c7c5999d3b44bc7c05ea03d2a5309d66b37ac421
Gerrit Henning <58300602+gh-max@users.noreply.github.com>
2023-12-16 18:19:03 +0200

    still working ch 3

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -7,41 +7,46 @@ import * as fs from 'node:fs'
 import * as path from 'node:path'
 import { parse } from 'csv-parse/sync'
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const user_records = parse(fs.readFileSync(path.join(__dirname, 'test.e2e.users.input.csv')), {
-  columns: true,
-  skip_empty_lines: true
-})
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const user_records = parse(
+  fs.readFileSync(path.join(__dirname, 'test.e2e.users.input.csv')),
+  {
+    columns: true,
+    skip_empty_lines: true,
+  },
+)
 
-const ticket_records = parse(fs.readFileSync(path.join(__dirname, 'test.e2e.tickets.input.csv')), {
-  columns: true,
-  skip_empty_lines: true
-})
+const ticket_records = parse(
+  fs.readFileSync(path.join(__dirname, 'test.e2e.tickets.input.csv')),
+  {
+    columns: true,
+    skip_empty_lines: true,
+  },
+)
 
 describe('new ticket creation test suite', () => {
-    for (const usr of user_records) {
-        it(`login user: ${usr.username}`, async () => {
-            await LoginPage.open()
-            await LoginPage.login(usr.username, usr.password)
-            await TicketDashboardPage.loaded()
-        })
+  for (const usr of user_records) {
+    it(`login user: ${usr.username}`, async () => {
+      await LoginPage.open()
+      await global.log(`login user: ${usr.username}`)
+      await global.log(Promise)
+      await LoginPage.login(usr.username, usr.password)
+      await TicketDashboardPage.loaded()
+    })
 
+    expect(TicketDashboardPage.assertUsernameDisplayed(usr.username))
+    expect(TicketDashboardPage.assertAddTicket())
+    expect(TicketPage.assertTicketCreateUrl())
 
-
-        expect (TicketDashboardPage.assertUsernameDisplayed(usr.username))
-        expect (TicketDashboardPage.assertAddTicket())
-        expect(TicketPage.assertTicketCreateUrl())
-
-        for (const ticket_record of ticket_records) {
-            it(`create ticket: ${ticket_record.summary}`, async () => {
-                expect(TicketPage.populate(ticket_record))
-            })
-        }
-
+    for (const ticket_record of ticket_records) {
+      it(`create ticket: ${ticket_record.summary}`, async () => {
+        expect(TicketPage.populate(ticket_record))
+      })
     }
+  }
 })
 
 // describe('T4SD Login form', () => {
@@ -50,10 +55,8 @@ describe('new ticket creation test suite', () => {
 
 //         let username='T4SYSAdmin'
 //         let password='_SYS'
-        
+
 //         await LoginPage.login(username, password)
- 
 
 //     })
 // })
-
